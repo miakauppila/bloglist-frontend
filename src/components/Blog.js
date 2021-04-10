218f2b8ec2f253814dfaf1408ff2d8dd659ed043
miakauppila <mia.kauppila@gmail.com>
2021-04-11 00:44:01 +0300

    refactor Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -3,7 +3,11 @@ import React, { useState } from 'react'
 const Blog = ({ blog, user, updateBlog, deleteBlog }) => {
   //console.log('Blog renders', blog)
   //console.log('user', user)
-  const [showBlog, setShowBlog] = useState(false)
+  const [showBlogDetails, setShowBlogDetails] = useState(false)
+
+  // empty display value '' does not affect display
+  const hideWhenVisible = { display: showBlogDetails ? 'none' : '' }
+  const showWhenVisible = { display: showBlogDetails ? '' : 'none' }
 
   const onRemoveHandler = (blog) => {
     if (window.confirm(`Remove blog ${blog.title} by ${blog.author}?`)) {
@@ -12,27 +16,22 @@ const Blog = ({ blog, user, updateBlog, deleteBlog }) => {
     }
   }
 
-  if (!showBlog) {
-    return (
-      <div className="blog">
-        {blog.title} {blog.author} <button onClick={() => setShowBlog(true)}>View</button>
+  return (
+    <div className="blog">
+      <div className="blog-header" style={hideWhenVisible} >
+        {blog.title} {blog.author} <button onClick={() => setShowBlogDetails(true)}>View</button>
       </div>
-    )
-  }
-  else {
-    return (
-      <ul className="blogStyle">
-        <li>{blog.title} {blog.author} <button onClick={() => setShowBlog(false)}>Hide</button></li>
+      <ul className="blog-details" style={showWhenVisible}>
+        <li>{blog.title} {blog.author} <button onClick={() => setShowBlogDetails(false)}>Hide</button></li>
         <li>{blog.url}</li>
-        <li>likes {blog.likes} <button onClick={() => updateBlog(blog)}>Like</button></li>
+        <li>likes {blog.likes} <button id="like-button" onClick={() => updateBlog(blog)}>Like</button></li>
         <li>{blog.user.name}</li>
         {blog.user.username === user.username ?
-          (<li> <button onClick={() => onRemoveHandler(blog)}>Remove</button></li>)
-          : null }
+          (<li> <button id="remove-button" onClick={() => onRemoveHandler(blog)}>Remove</button></li>)
+          : null}
       </ul>
-    )
-  }
-
+    </div>
+  )
 }
 
 export default Blog
